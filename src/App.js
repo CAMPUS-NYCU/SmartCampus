021f0549c3c988b4f38fd16f85afd190acf1d262
Kamigami55 <eason@easonchang.com>
2019-10-25 15:35:42 +0800

    Add Material-UI library (ver 4.5.1)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 import { INDEX_PATH, MAP_PATH, LOGIN_PATH } from './constants/pageUrls';
 import MapPage from './pages/MapPage';
@@ -13,35 +14,38 @@ import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to={INDEX_PATH}>Home</Link>
-            </li>
-            <li>
-              <Link to={MAP_PATH}>Map</Link>
-            </li>
-            <li>
-              <Link to={LOGIN_PATH}>Login</Link>
-            </li>
-          </ul>
-        </nav>
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to={INDEX_PATH}>Home</Link>
+              </li>
+              <li>
+                <Link to={MAP_PATH}>Map</Link>
+              </li>
+              <li>
+                <Link to={LOGIN_PATH}>Login</Link>
+              </li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path={INDEX_PATH} exact>
-            <IndexPage />
-          </Route>
-          <Route path={MAP_PATH} exact>
-            <MapPage />
-          </Route>
-          <Route path={LOGIN_PATH} exact>
-            <LoginPage />
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+          <Switch>
+            <Route path={INDEX_PATH} exact>
+              <IndexPage />
+            </Route>
+            <Route path={MAP_PATH} exact>
+              <MapPage />
+            </Route>
+            <Route path={LOGIN_PATH} exact>
+              <LoginPage />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
