2123cf2a6523708190e0cbb7a440dd5badd0c52c
Kamigami55 <eason@easonchang.com>
2019-10-29 15:26:08 +0800

    Add initial UI to login page
    登入頁加入初版 UI
    
    尚未串接功能

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,69 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+
+import mapBackgroundImage from '../assets/images/map-bg-image.png';
+
+const useStyles = makeStyles((theme) => ({
+  page: {
+    height: '100vh',
+    width: '100vw',
+    // 白色透明 overlay
+    background: `linear-gradient(0deg,rgba(255,255,255,0.7),rgba(255,255,255,0.7)),url(${mapBackgroundImage})`,
+    // backgroundImage: `url(${mapBackgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    padding: theme.spacing(4),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    minWidth: 275,
+  },
+}));
+
 export default function LoginPage() {
+  const classes = useStyles();
+
   return (
-    <p>LoginPage</p>
+    <div className={classes.page}>
+      <Card className={classes.card}>
+        <CardContent>
+          <Box m={2}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Sign in with Google
+            </Button>
+          </Box>
+          <Box m={2}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Sign in with Facebook
+            </Button>
+          </Box>
+          <Box m={2}>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Sign in with NCTU Portal
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </div>
   );
 }
