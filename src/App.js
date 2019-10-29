835033da34890e665b0a6265015ecfddf1d4504e
Kamigami55 <eason@easonchang.com>
2019-10-29 15:25:07 +0800

    Add material-ui theme
    全站套用 material-ui theme（尚未客製化）
    
    ref: https://material-ui.com/customization/theming/#theming

--- src/App.js
+++ src/App.js
@@ -5,46 +5,53 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import { INDEX_PATH, MAP_PATH, LOGIN_PATH } from './constants/pageUrls';
 import MapPage from './pages/MapPage';
 import IndexPage from './pages/IndexPage';
 import LoginPage from './pages/LoginPage';
 
+const theme = createMuiTheme({
+});
+
 function App() {
   return (
     <>
-      <CssBaseline />
-      <BrowserRouter>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to={INDEX_PATH}>Home</Link>
-              </li>
-              <li>
-                <Link to={MAP_PATH}>Map</Link>
-              </li>
-              <li>
-                <Link to={LOGIN_PATH}>Login</Link>
-              </li>
-            </ul>
-          </nav>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <BrowserRouter>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to={INDEX_PATH}>Home</Link>
+                </li>
+                <li>
+                  <Link to={MAP_PATH}>Map</Link>
+                </li>
+                <li>
+                  <Link to={LOGIN_PATH}>Login</Link>
+                </li>
+              </ul>
+            </nav>
 
-          <Switch>
-            <Route path={INDEX_PATH} exact>
-              <IndexPage />
-            </Route>
-            <Route path={MAP_PATH} exact>
-              <MapPage />
-            </Route>
-            <Route path={LOGIN_PATH} exact>
-              <LoginPage />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
+            <Switch>
+              <Route path={INDEX_PATH} exact>
+                <IndexPage />
+              </Route>
+              <Route path={MAP_PATH} exact>
+                <MapPage />
+              </Route>
+              <Route path={LOGIN_PATH} exact>
+                <LoginPage />
+              </Route>
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   );
 }
