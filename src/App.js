9ecbe481f135f5fb8005c2ef5edbbc63cc70c028
Kamigami55 <eason@easonchang.com>
2019-10-29 20:28:24 +0800

    Remove useless nav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {
   BrowserRouter,
   Switch,
   Route,
-  Link,
 } from 'react-router-dom';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -23,33 +22,17 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <BrowserRouter>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to={INDEX_PATH}>Home</Link>
-                </li>
-                <li>
-                  <Link to={MAP_PATH}>Map</Link>
-                </li>
-                <li>
-                  <Link to={LOGIN_PATH}>Login</Link>
-                </li>
-              </ul>
-            </nav>
-
-            <Switch>
-              <Route path={INDEX_PATH} exact>
-                <IndexPage />
-              </Route>
-              <Route path={MAP_PATH} exact>
-                <MapPage />
-              </Route>
-              <Route path={LOGIN_PATH} exact>
-                <LoginPage />
-              </Route>
-            </Switch>
-          </div>
+          <Switch>
+            <Route path={INDEX_PATH} exact>
+              <IndexPage />
+            </Route>
+            <Route path={MAP_PATH} exact>
+              <MapPage />
+            </Route>
+            <Route path={LOGIN_PATH} exact>
+              <LoginPage />
+            </Route>
+          </Switch>
         </BrowserRouter>
       </ThemeProvider>
     </>
