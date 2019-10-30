492e922210db73c466eb297036fcba28861f190f
Kamigami55 <eason@easonchang.com>
2019-10-30 18:09:43 +0800

    Add MapBox to map page
    - 安裝了 react-map-gl 套件
    - map頁加入了地圖，可拖動放大縮小，預設地點設在交大工三
    - ReactMapGL 需要 MapBox access token (public)，token 放在 .env 環境變數檔案內
    - 須在專案根目錄新增 .env 環境變數檔，設定 REACT_APP_MAPBOX_ACCESS_TOKEN

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -5,30 +5,15 @@ import Fab from '@material-ui/core/Fab';
 import HelpIcon from '@material-ui/icons/Help';
 
 import NavBottom from './components/NavBottom';
-import mapBackgroundImage from '../../assets/images/map-bg-image.png';
 import HowToUseDialog from './components/HowToUseDialog';
 import SearchBar from './components/SearchBar';
+import Map from './components/Map';
 
 const useStyles = makeStyles((theme) => ({
-  map: {
-    position: 'relative',
-    height: 'calc(100vh - 56px)',
-    width: '100vw',
-    // 白色透明 overlay
-    background: `url(${mapBackgroundImage})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-  },
   fab: {
     position: 'absolute',
-    bottom: theme.spacing(2),
+    bottom: 56 + theme.spacing(4),
     right: theme.spacing(2),
-  },
-  closeButton: {
-    position: 'absolute',
-    right: theme.spacing(1),
-    top: theme.spacing(1),
-    color: theme.palette.grey[500],
   },
 }));
 
@@ -41,16 +26,16 @@ export default function MapPage() {
 
   return (
     <div>
-      <div className={classes.map}>
-        <Fab
-          color="primary"
-          aria-label="how-to-use"
-          className={classes.fab}
-          onClick={handleOpenHelp}
-        >
-          <HelpIcon />
-        </Fab>
-      </div>
+      <Map />
+
+      <Fab
+        color="primary"
+        aria-label="how-to-use"
+        className={classes.fab}
+        onClick={handleOpenHelp}
+      >
+        <HelpIcon />
+      </Fab>
 
       <SearchBar />
 
