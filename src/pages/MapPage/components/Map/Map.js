ed6c644ec11192737ffd328281879ae877b493bc
Kamigami55 <eason@easonchang.com>
2019-10-31 13:06:16 +0800

    Refactor <Map/> to functional component & Add NavigationControl to map
    - Map 元件改為 functional component
    - 地圖右上角加入zoom按鈕

--- src/pages/MapPage/components/Map/Map.js
+++ src/pages/MapPage/components/Map/Map.js
@@ -1,30 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, { NavigationControl } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { makeStyles } from '@material-ui/core/styles';
 
 import { REACT_APP_MAPBOX_ACCESS_TOKEN } from '../../../../constants/envValues';
 
-class Map extends Component {
-  state = {
-    viewport: {
-      width: '100vw',
-      height: 'calc(100vh - 56px)',
-      latitude: 24.7872616, // 交大經緯度
-      longitude: 120.9969249,
-      zoom: 17,
-    },
+const useStyles = makeStyles((theme) => ({
+  navigation: {
+    position: 'absolute',
+    top: 100,
+    right: 0,
+    margin: theme.spacing(1),
+  },
+}));
+
+function Map() {
+  const classes = useStyles();
+  const INITIAL_VIEWPORT = {
+    width: '100vw',
+    height: 'calc(100vh - 56px)',
+    longitude: 120.9969249, // 交大經緯度
+    latitude: 24.7872616,
+    zoom: 17,
   };
 
-  render() {
-    return (
-      <ReactMapGL
-        onViewportChange={(viewport) => this.setState({ viewport })}
-        mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
-        {...this.state.viewport}
-      />
-    );
-  }
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
+    >
+      <div className={classes.navigation}>
+        <NavigationControl />
+      </div>
+    </ReactMapGL>
+  );
 }
 
 export default Map;
