8b0437312c365132c6ccd13a4454d6700ba67aa3
Kamigami55 <eason@easonchang.com>
2019-12-18 04:55:37 +0800

    完成 MissionStep3 內容（尚未串接功能）

--- src/pages/MapPage/components/Mission/MissionDrawer/MissionStep3/MissionStep3.js
+++ src/pages/MapPage/components/Mission/MissionDrawer/MissionStep3/MissionStep3.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-MissionStep3.propTypes = {
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
 
-};
+const Missions = [
+  {
+    id: 1,
+    name: '無障礙設施',
+  },
+  {
+    id: 2,
+    name: '道路障礙',
+  },
+];
 
-function MissionStep3(props) {
+function MissionStep3() {
   return (
-    <div />
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography>
+      3. 選擇你想要的標注任務
+        </Typography>
+      </Grid>
+      {Missions.map((mission) => (
+        <Grid id={mission.id} item xs={4}>
+          <Button
+            variant="contained"
+            fullWidth
+            size="small"
+          >
+            {mission.name}
+          </Button>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
