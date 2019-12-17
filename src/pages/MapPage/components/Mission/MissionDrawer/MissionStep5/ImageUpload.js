d97e4bb01abf9e087e9e176c10a99484fd634ea6
Kamigami55 <eason@easonchang.com>
2019-12-18 05:57:35 +0800

    完成 MissionStep5 內容（尚未串接功能）

---
+++ src/pages/MapPage/components/Mission/MissionDrawer/MissionStep5/ImageUpload.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+
+function ImageUpload() {
+  return (
+    <Button
+      variant="contained"
+    >
+      選擇照片
+    </Button>
+  );
+}
+
+export default ImageUpload;
--- src/pages/MapPage/components/Mission/MissionDrawer/MissionStep5/MissionStep5.js
+++ src/pages/MapPage/components/Mission/MissionDrawer/MissionStep5/MissionStep5.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-MissionStep5.propTypes = {
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
-};
+import ImageUpload from './ImageUpload'
 
-function MissionStep5(props) {
+function MissionStep5() {
   return (
-    <div />
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Typography>
+          5. 關於這地點的描述？
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          multiline
+          variant="outlined"
+          rows={5}
+          placeholder="描述..."
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>
+          6. 相關的照片
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <ImageUpload />
+      </Grid>
+    </Grid>
   );
 }
 
