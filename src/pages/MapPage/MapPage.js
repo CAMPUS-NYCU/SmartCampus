b9bd70a7def396a88db8ab18f6a897d7a1183c59
Kamigami55 <eason@easonchang.com>
2019-12-18 09:23:08 +0800

    拿掉 Help FAB

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { useSnackbar } from 'notistack';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import HelpIcon from '@material-ui/icons/Help';
-
 import HowToUseDialog from './components/HowToUseDialog';
 import SearchBar from './components/SearchBar';
 import Map from './components/Map';
@@ -13,16 +9,7 @@ import MissionFab from './components/Mission/MissionFab';
 import MissionDrawer from './components/Mission/MissionDrawer';
 import ProfileDialog from './components/ProfileDialog/ProfileDialog';
 
-const useStyles = makeStyles((theme) => ({
-  fab: {
-    position: 'absolute',
-    bottom: theme.spacing(4),
-    right: theme.spacing(2),
-  },
-}));
-
 export default function MapPage() {
-  const classes = useStyles();
   const { enqueueSnackbar } = useSnackbar();
 
   const howToUseDialogControl = useModal();
@@ -44,16 +31,6 @@ export default function MapPage() {
         onClose={missionDrawer.setClose}
         handleAddMissionComplete={handleAddMissionComplete}
       />
-
-      <Fab
-        color="primary"
-        aria-label="how-to-use"
-        size="small"
-        className={classes.fab}
-        onClick={howToUseDialogControl.setOpen}
-      >
-        <HelpIcon />
-      </Fab>
 
       <SearchBar
         menuControls={{
