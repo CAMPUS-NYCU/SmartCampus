cbfbe4801757128fd497c7092bd5ffe4bebb210a
Kamigami55 <eason@easonchang.com>
2019-12-18 09:27:46 +0800

    拿掉 MapGL 的 zoom 按鈕

--- src/pages/MapPage/components/Map/Map.js
+++ src/pages/MapPage/components/Map/Map.js
@@ -1,23 +1,11 @@
 import React, { useState } from 'react';
 
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
+import ReactMapGL from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-
-import { makeStyles } from '@material-ui/core/styles';
 
 import { REACT_APP_MAPBOX_ACCESS_TOKEN } from '../../../../constants/envValues';
 
-const useStyles = makeStyles((theme) => ({
-  navigation: {
-    position: 'absolute',
-    top: 100,
-    right: 0,
-    margin: theme.spacing(1),
-  },
-}));
-
 function Map() {
-  const classes = useStyles();
   const INITIAL_VIEWPORT = {
     width: '100vw',
     height: '100vh',
@@ -34,11 +22,7 @@ function Map() {
       onViewportChange={setViewport}
       mapStyle="mapbox://styles/mapbox/outdoors-v10"
       mapboxApiAccessToken={REACT_APP_MAPBOX_ACCESS_TOKEN}
-    >
-      <div className={classes.navigation}>
-        <NavigationControl />
-      </div>
-    </ReactMapGL>
+    />
   );
 }
 
