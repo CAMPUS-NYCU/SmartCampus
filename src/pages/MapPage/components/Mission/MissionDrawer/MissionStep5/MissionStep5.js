7ae2be1f6d450406fd7c3c273a5e2c7c76a79bdc
Kamigami55 <eason@easonchang.com>
2019-12-25 05:25:32 +0800

    串接動態 moreDescriptionText

--- src/pages/MapPage/components/Mission/MissionDrawer/MissionStep5/MissionStep5.js
+++ src/pages/MapPage/components/Mission/MissionDrawer/MissionStep5/MissionStep5.js
@@ -4,9 +4,15 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 
-import ImageUpload from './ImageUpload'
+import ImageUpload from './ImageUpload';
+import { useMissionValue } from '../../../../contexts/MissionContext';
 
 function MissionStep5() {
+  const {
+    moreDescriptionText,
+    handleChangeMoreDescriptionText,
+  } = useMissionValue();
+
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
@@ -16,11 +22,14 @@ function MissionStep5() {
       </Grid>
       <Grid item xs={12}>
         <TextField
+          name="moreDescriptionText"
           multiline
           variant="outlined"
           rows={5}
           placeholder="描述..."
           fullWidth
+          value={moreDescriptionText}
+          onChange={handleChangeMoreDescriptionText}
         />
       </Grid>
       <Grid item xs={12}>
