44c9787f3ce1a6b57529674a88bbb2954f55281c
L4ll1p0p <chen1082yo@gmail.com>
2020-05-26 21:43:04 +0800

    eslint fix for tagDetail

--- src/pages/MapPage/TagDetailDialog.js
+++ src/pages/MapPage/TagDetailDialog.js
@@ -8,10 +8,8 @@ import { makeStyles } from '@material-ui/core/styles'
 import CloseIcon from '@material-ui/icons/Close'
 import Drawer from '@material-ui/core/Drawer'
 import { Box } from '@material-ui/core'
-import HandicapIcon from '../../assets/images/handicap-icon.svg'
 import Button from '@material-ui/core/Button'
-
-
+import HandicapIcon from '../../assets/images/handicap-icon.svg'
 
 TagDetailDialog.propTypes = {
   activeTag: PropTypes.object,
@@ -54,57 +52,97 @@ function TagDetailDialog(props) {
       </DialogTitle>
 
       {activeTag ? (
-        <Box display="flex" flexDirection="column" alignItems="center">
-          <Box display="flex" alignItems="center" flexDirection="row" justifyContent="space-around" width='100%'>
-            <img src={HandicapIcon}></img>
+        <Box display='flex' flexDirection='column' alignItems='center'>
+          <Box
+            display='flex'
+            alignItems='center'
+            flexDirection='row'
+            justifyContent='space-around'
+            width='100%'
+          >
+            <img src={HandicapIcon} alt='' />
             <Typography>{activeTag.missionName}</Typography>
             <Typography>{activeTag.discoveryName}</Typography>
-            <Button variant="contained" style={{ background: '#dce775', cursor: 'default' }} disableElevation disableRipple>
+            <Button
+              variant='contained'
+              style={{ background: '#dce775', cursor: 'default' }}
+              disableElevation
+              disableRipple
+            >
               待解決
             </Button>
           </Box>
-          <Box display="flex" alignItems="center" flexDirection="row" style={{ margin: '4vw 0 0 0' }}>
+          <Box
+            display='flex'
+            alignItems='center'
+            flexDirection='row'
+            style={{ margin: '4vw 0 0 0' }}
+          >
             <div
               style={{
-                width: '100vw', height: '80vw', backgroundSize: 'cover', backgroundPosition: 'center',
-                backgroundImage: 'url(https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600)'
+                width: '100vw',
+                height: '80vw',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundImage:
+                  'url(https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600)'
               }}
             />
             {/* <div
               style={{ width: '100vw', height: '80vw', overflowY: 'scroll', backgroundSize: 'cover', backgroundPosition: 'center', backgroundImage: 'url(https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600)' }}
             /> */}
           </Box>
-          <Box display="flex" alignItems="center" flexDirection="row" justifyContent="space-between" m={2} width="80vw">
-            <Button variant="contained">
-              更改狀態
-              </Button>
-            <Box display="flex" flexDirection="column" alignItems="flex-end">
+          <Box
+            display='flex'
+            alignItems='center'
+            flexDirection='row'
+            justifyContent='space-between'
+            m={2}
+            width='80vw'
+          >
+            <Button variant='contained'>更改狀態</Button>
+            <Box display='flex' flexDirection='column' alignItems='flex-end'>
               <p style={{ fontSize: '0.5em' }}>狀態編輯紀錄</p>
-              <p style={{ fontSize: '0.5em' }}>{activeTag.detail.lastUpdateTimeNew}</p>
+              <p style={{ fontSize: '0.5em' }}>
+                {activeTag.detail.lastUpdateTimeNew}
+              </p>
             </Box>
           </Box>
-          <div style={{ width: '80vw', borderTop: 'solid 0.5px lightgray', borderBottom: 'solid 0.5px lightgray', paddingBottom: '2' }}>
-            {activeTag.detail.description?(
-              <p>
-              {activeTag.detail.description}
-            </p>
-            ):(
-              <p>
-              無描述
-            </p>
+          <div
+            style={{
+              width: '80vw',
+              borderTop: 'solid 0.5px lightgray',
+              borderBottom: 'solid 0.5px lightgray',
+              paddingBottom: '2'
+            }}
+          >
+            {activeTag.detail.description ? (
+              <p>{activeTag.detail.description}</p>
+            ) : (
+              <p>無描述</p>
             )}
-            <Box display="flex" justifyContent="flex-end">
-              <p style={{ fontSize: '0.5em' }}>{activeTag.detail.createTimeNew}</p>
+            <Box display='flex' justifyContent='flex-end'>
+              <p style={{ fontSize: '0.5em' }}>
+                {activeTag.detail.createTimeNew}
+              </p>
             </Box>
           </div>
-          <Box display="flex" justifyContent='flex-end' alignItems="center" width='80%' m={2}>
+          <Box
+            display='flex'
+            justifyContent='flex-end'
+            alignItems='center'
+            width='80%'
+            m={2}
+          >
             <p style={{ fontSize: '0.7em' }}>86人贊同此問題待處理</p>
-            <Button variant="contained" style={{ marginLeft: '8px' }}>贊同</Button>
+            <Button variant='contained' style={{ marginLeft: '8px' }}>
+              贊同
+            </Button>
           </Box>
         </Box>
       ) : (
-          <Typography>讀取中...</Typography>
-        )}
+        <Typography>讀取中...</Typography>
+      )}
     </Drawer>
   )
 }
