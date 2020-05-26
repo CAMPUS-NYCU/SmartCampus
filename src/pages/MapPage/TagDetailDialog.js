2d546a71dc6b689eb42beb0a05fe8c8466e39387
L4ll1p0p <chen1082yo@gmail.com>
2020-05-26 10:57:20 +0800

    更改月份bug

--- src/pages/MapPage/TagDetailDialog.js
+++ src/pages/MapPage/TagDetailDialog.js
@@ -4,15 +4,13 @@ import PropTypes from 'prop-types'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import DialogContent from '@material-ui/core/DialogContent'
-import Dialog from '@material-ui/core/Dialog'
 import { makeStyles } from '@material-ui/core/styles'
 import CloseIcon from '@material-ui/icons/Close'
 import Drawer from '@material-ui/core/Drawer'
 import { Box } from '@material-ui/core'
 import HandicapIcon from '../../assets/images/handicap-icon.svg'
 import Button from '@material-ui/core/Button'
-import DialogContentText from '@material-ui/core/DialogContentText'
+
 
 
 TagDetailDialog.propTypes = {
@@ -32,14 +30,9 @@ const useStyles = makeStyles((theme) => ({
   }
 }))
 
-
-
-
-
 function TagDetailDialog(props) {
   const { activeTag, onClose } = props
   const classes = useStyles()
-  console.log(activeTag)
   return (
     <Drawer
       anchor='bottom'
--- src/pages/MapPage/hooks/useTagList.js
+++ src/pages/MapPage/hooks/useTagList.js
@@ -35,30 +35,43 @@ const generateTime = (time) => {
   switch (times[1]) {
     case 'Jan':
       month = 1
+      break
     case 'Feb':
       month = 2
+      break
     case 'Mar':
       month = 3
+      break
     case 'Apr':
       month = 4
+      break
     case 'May':
       month = 5
+      break
     case 'Jun':
       month = 6
+      break
     case 'Jul':
       month = 7
+      break
     case 'Aug':
       month = 8
+      break
     case 'Sep':
       month = 9
+      break
     case 'Oct':
       month = 10
+      break
     case 'Nov':
       month = 11
+      break
     case 'Dec':
       month = 12
+      break
     default:
       month = 0
+      break
   }
   return `${times[3]}-${month}-${times[2]} ${times[4]}`
 }
