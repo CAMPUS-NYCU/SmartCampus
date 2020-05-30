27eb71ace7b7d87fd1e79210878045c0d9323f50
Eason Chang <samkami@icloud.com>
2020-05-30 18:37:38 +0800

    安裝console addon
    在action panel會顯示元件的console log，方便debug
    
    參考：
    https://github.com/storybookjs/storybook-addon-console

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,15 +1,23 @@
 import { addParameters, addDecorator } from '@storybook/react'
 
-import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
-import { withA11y } from '@storybook/addon-a11y'
+// ========================================
+
+// 啟用 console addon
+import '@storybook/addon-console'
+
+// ========================================
 
 // 讓viewport addon有更多裝置大小可以點
 // ref: https://github.com/storybookjs/storybook/tree/master/addons/viewport#use-detailed-set-of-devices
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 addParameters({
   viewport: {
     viewports: INITIAL_VIEWPORTS
   }
 })
 
-// a11y addon
+// ========================================
+
+// 啟用 a11y addon
+import { withA11y } from '@storybook/addon-a11y'
 addDecorator(withA11y)
