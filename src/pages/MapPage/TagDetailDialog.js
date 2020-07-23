a70796a7f5716eefef0a002193801df5fffae359
L4ll1p0p <chen1082yo@gmail.com>
2020-07-23 11:42:40 +0800

    tagdetail picture be ramaining space

--- src/pages/MapPage/TagDetailDialog.js
+++ src/pages/MapPage/TagDetailDialog.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import DialogTitle from '@material-ui/core/DialogTitle'
@@ -45,132 +45,132 @@ function TagDetailDialog(props) {
       open={activeTag}
       onClose={onClose}
     >
-      <DialogTitle disableTypography onClose={onClose}>
-        <Typography variant='h5'>
-          {activeTag ? activeTag.discoveryName : '詳細資訊'}
-        </Typography>
-        <IconButton
-          aria-label='close'
-          className={classes.closeButton}
-          onClick={onClose}
-        >
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
+      <div style={{ height: '85vh', display: 'flex', flexDirection: 'column' }}>
+        <DialogTitle disableTypography onClose={onClose}>
+          <Typography variant='h5'>
+            {activeTag ? activeTag.discoveryName : '詳細資訊'}
+          </Typography>
+          <IconButton
+            aria-label='close'
+            className={classes.closeButton}
+            onClick={onClose}
+          >
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
 
-      {activeTag && detail ? (
-        <Box display='flex' flexDirection='column' alignItems='center'>
+        {activeTag && detail ? (
           <Box
             display='flex'
+            flexDirection='column'
             alignItems='center'
-            flexDirection='row'
-            justifyContent='space-between'
-            width='100%'
+            style={{ flexGrow: '1' }}
           >
             <Box
               display='flex'
               alignItems='center'
-              justifyContent='space-around'
-              width='50%'
+              flexDirection='row'
+              justifyContent='space-between'
+              width='100%'
             >
-              <img src={HandicapIcon} alt='' />
-              <Typography>{activeTag.missionName}</Typography>
-              <Typography>圖書館</Typography>
+              <Box
+                display='flex'
+                alignItems='center'
+                justifyContent='space-around'
+                width='50%'
+              >
+                <img src={HandicapIcon} alt='' />
+                <Typography>{activeTag.missionName}</Typography>
+                <Typography>圖書館</Typography>
+              </Box>
+              <Button
+                variant='contained'
+                style={{ background: '#dce775', cursor: 'default' }}
+                disableElevation
+                disableRipple
+              >
+                待解決
+              </Button>
             </Box>
-            <Button
-              variant='contained'
-              style={{ background: '#dce775', cursor: 'default' }}
-              disableElevation
-              disableRipple
-            >
-              待解決
-            </Button>
-          </Box>
-          <Box
-            display='flex'
-            alignItems='center'
-            flexDirection='row'
-            justifyContent='flex-start'
-            style={{ margin: '4vw 0 0 0', width: '100vw' }}
-          >
             <div
               style={{
                 width: '100vw',
-                height: '40vh',
+                margin: '4vw 0 0 0',
+                flexGrow: '1',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundImage:
                   'url(https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600)'
               }}
             />
-          </Box>
-          <Box
-            display='flex'
-            alignItems='center'
-            flexDirection='row'
-            justifyContent='space-between'
-            m={2}
-            width='80vw'
-          >
-            <Button variant='contained'>更改狀態</Button>
-            <Box display='flex' flexDirection='column' alignItems='flex-end'>
-              <Box
-                className={classes.clickableFont}
-                style={{ textDecoration: 'underline', cursor: 'pointer' }}
-                m={0.5}
-              >
-                狀態編輯紀錄
+            <Box
+              display='flex'
+              alignItems='center'
+              flexDirection='row'
+              justifyContent='space-between'
+              m={2}
+              width='80vw'
+            >
+              <Button variant='contained'>更改狀態</Button>
+              <Box display='flex' flexDirection='column' alignItems='flex-end'>
+                <Box
+                  className={classes.clickableFont}
+                  style={{ textDecoration: 'underline', cursor: 'pointer' }}
+                  m={0.5}
+                >
+                  狀態編輯紀錄
+                </Box>
+                <Box className={classes.clickableFont} m={0.5}>
+                  {detail.newLastUpdateTime}
+                </Box>
               </Box>
+            </Box>
+            <div
+              style={{
+                width: '80vw',
+                borderTop: 'solid 0.5px lightgray',
+                borderBottom: 'solid 0.5px lightgray',
+                paddingBottom: '2'
+              }}
+            >
+              {detail.description ? (
+                <Box
+                  my={2}
+                  textOverflow='ellipsis'
+                  component='div'
+                  overflow='hidden'
+                  height='4.5em'
+                >
+                  {detail.description}
+                </Box>
+              ) : (
+                  <p>無描述</p>
+                )}
+              <Box display='flex' justifyContent='flex-end'>
+                <Box className={classes.clickableFont} m={0.5}>
+                  {detail.newCreateTime}
+                </Box>
+              </Box>
+            </div>
+            <Box
+              display='flex'
+              justifyContent='flex-end'
+              alignItems='center'
+              width='80%'
+              m={2}
+            >
               <Box className={classes.clickableFont} m={0.5}>
-                {detail.newLastUpdateTime}
+                86人贊同此問題待處理
               </Box>
+              <Button variant='contained' style={{ marginLeft: '8px' }}>
+                贊同
+              </Button>
             </Box>
           </Box>
-          <div
-            style={{
-              width: '80vw',
-              borderTop: 'solid 0.5px lightgray',
-              borderBottom: 'solid 0.5px lightgray',
-              paddingBottom: '2'
-            }}
-          >
-            {detail.description ? (
-              <Box
-                my={2}
-                textOverflow='ellipsis'
-                component='div'
-                overflow='hidden'
-                height='4.5em'
-              >
-                {detail.description}
-              </Box>
-            ) : (
-              <p>無描述</p>
-            )}
-            <Box display='flex' justifyContent='flex-end'>
-              <Box className={classes.clickableFont} m={0.5}>
-                {detail.newCreateTime}
-              </Box>
-            </Box>
-          </div>
-          <Box
-            display='flex'
-            justifyContent='flex-end'
-            alignItems='center'
-            width='80%'
-            m={2}
-          >
-            <Box className={classes.clickableFont} m={0.5}>
-              86人贊同此問題待處理
-            </Box>
-            <Button variant='contained' style={{ marginLeft: '8px' }}>
-              贊同
-            </Button>
-          </Box>
-        </Box>
-      ) : (
-        <Typography>讀取中...</Typography>
-      )}
+        ) : (
+            <Typography>讀取中...</Typography>
+          )}
+      </div>
     </Drawer>
   )
 }
