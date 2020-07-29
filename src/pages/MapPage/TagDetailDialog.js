c6df99de8db198ad1798aba2a36f674de91a66ec
L4ll1p0p <chen1082yo@gmail.com>
2020-07-29 12:42:57 +0800

    change state button style

--- src/pages/MapPage/TagDetailDialog.js
+++ src/pages/MapPage/TagDetailDialog.js
@@ -8,9 +8,9 @@ import CloseIcon from '@material-ui/icons/Close'
 import Drawer from '@material-ui/core/Drawer'
 import { Box } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
+import { Lightbox } from 'react-modal-image'
 import HandicapIcon from '../../assets/images/handicap-icon.svg'
 import useTagDetail from './hooks/useTagDetail'
-import { Lightbox } from "react-modal-image";
 
 TagDetailDialog.propTypes = {
   activeTag: PropTypes.object,
@@ -31,15 +31,15 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '0.8em',
     color: 'gray'
   },
-  largeImage:{
-    maxWidth:'100vw'
+  changeButton: {
+    borderRadius: '40%'
   }
 }))
 
 function TagDetailDialog(props) {
   const { activeTag, onClose } = props
   const detail = useTagDetail(activeTag.id)
-  const [ largeImg, setLargeImg ] = useState(null)
+  const [largeImg, setLargeImg] = useState(null)
   const classes = useStyles()
   return (
     <>
@@ -49,7 +49,9 @@ function TagDetailDialog(props) {
         open={activeTag}
         onClose={onClose}
       >
-        <div style={{ height: '85vh', display: 'flex', flexDirection: 'column' }}>
+        <div
+          style={{ height: '85vh', display: 'flex', flexDirection: 'column' }}
+        >
           <DialogTitle disableTypography onClose={onClose}>
             <Typography variant='h5'>
               {activeTag ? activeTag.discoveryName : '詳細資訊'}
@@ -87,26 +89,41 @@ function TagDetailDialog(props) {
                   <Typography>{activeTag.missionName}</Typography>
                   <Typography>圖書館</Typography>
                 </Box>
-                
-                
+
                 <div
-                  style={{ cursor: 'default', width:'100px', height:'36px',borderTop:'18px solid #dce775', borderBottom:'18px solid #dce775', borderLeft:'18px solid transparent', textAlign:'center'}}
+                  style={{
+                    cursor: 'default',
+                    width: '100px',
+                    height: '36px',
+                    borderTop: '18px solid #dce775',
+                    borderBottom: '18px solid #dce775',
+                    borderLeft: '18px solid transparent',
+                    textAlign: 'center'
+                  }}
                 >
-                  <Typography style={{position:'relative', top:'-9px'}}>圖書館</Typography>
-              </div>
+                  <Typography style={{ position: 'relative', top: '-9px' }}>
+                    圖書館
+                  </Typography>
+                </div>
               </Box>
-              <div style={{
-                width: '100vw',
-                margin: '4vw 0 0 0',
-                height: '100%',
-                flexGrow: '1',
-                overflowX: 'scroll',
-                overflowY: 'hidden',
-                display: 'flex',
-                flexDirection: 'row'
-              }}>
+              <div
+                style={{
+                  width: '100vw',
+                  margin: '4vw 0 0 0',
+                  height: '100%',
+                  flexGrow: '1',
+                  overflowX: 'scroll',
+                  overflowY: 'hidden',
+                  display: 'flex',
+                  flexDirection: 'row'
+                }}
+              >
                 <div
-                  onClick={()=>setLargeImg('https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600')}
+                  onClick={() =>
+                    setLargeImg(
+                      'https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600'
+                    )
+                  }
                   style={{
                     width: '80vw',
                     height: '100%',
@@ -119,9 +136,12 @@ function TagDetailDialog(props) {
                       'url(https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600)'
                   }}
                 />
-
                 <div
-                  onClick={()=>setLargeImg('https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600')}
+                  onClick={() =>
+                    setLargeImg(
+                      'https://pgw.udn.com.tw/gw/photo.php?u=https://uc.udn.com.tw/photo/2019/09/18/1/6835002.jpg&x=0&y=0&sw=0&sh=0&exp=3600'
+                    )
+                  }
                   style={{
                     width: '80vw',
                     height: '100%',
@@ -143,15 +163,31 @@ function TagDetailDialog(props) {
                 m={2}
                 width='80vw'
               >
-                <Button variant='contained'>更改狀態</Button>
-                <Box display='flex' flexDirection='column' alignItems='flex-end'>
+                <Button
+                  style={{
+                    background: '#FDCC4F',
+                    /* Primary_light */
+                    border: '1px solid #FFEDC0',
+                    borderRadius: '20px',
+                    boxShadow: 'inset 0px 2px 4px rgba(0, 0, 0, 0.14)',
+                    filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))'
+                  }}
+                  variant='contained'
+                >
+                  更改狀態
+                </Button>
+                <Box
+                  display='flex'
+                  flexDirection='column'
+                  alignItems='flex-end'
+                >
                   <Box
                     className={classes.clickableFont}
                     style={{ textDecoration: 'underline', cursor: 'pointer' }}
                     m={0.5}
                   >
                     狀態編輯紀錄
-                </Box>
+                  </Box>
                   <Box className={classes.clickableFont} m={0.5}>
                     {detail.newLastUpdateTime}
                   </Box>
@@ -176,8 +212,8 @@ function TagDetailDialog(props) {
                     {detail.description}
                   </Box>
                 ) : (
-                    <p>無描述</p>
-                  )}
+                  <p>無描述</p>
+                )}
                 <Box display='flex' justifyContent='flex-end'>
                   <Box className={classes.clickableFont} m={0.5}>
                     {detail.newCreateTime}
@@ -193,21 +229,25 @@ function TagDetailDialog(props) {
               >
                 <Box className={classes.clickableFont} m={0.5}>
                   86人贊同此問題待處理
-              </Box>
+                </Box>
                 <Button variant='contained' style={{ marginLeft: '8px' }}>
                   贊同
-              </Button>
+                </Button>
               </Box>
             </Box>
           ) : (
-              <Typography>讀取中...</Typography>
-            )}
+            <Typography>讀取中...</Typography>
+          )}
         </div>
       </Drawer>
-      {largeImg&&(<Lightbox large={largeImg} onClose={()=>setLargeImg(null)} hideDownload hideZoom></Lightbox>)}
-      {/* <Dialog classes={classes.largeImage} open={largeImg} onClose={()=>setLargeImg(null)}>
-        <div style={{backgroundImage:`url(${largeImg})`, width:'100vw', height:'60vh'}}></div>
-      </Dialog> */}
+      {largeImg && (
+        <Lightbox
+          large={largeImg}
+          onClose={() => setLargeImg(null)}
+          hideDownload
+          hideZoom
+        />
+      )}
     </>
   )
 }
