036b03f38a859ae2ee1655203509082200e4aee8
L4ll1p0p <chen1082yo@gmail.com>
2021-01-09 16:41:19 +0800

    disable guide page 2 listitem

--- src/pages/MapPage/components/GuidePage/GuidePageStep2.js
+++ src/pages/MapPage/components/GuidePage/GuidePageStep2.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
-import { Typography, List, ListItem, ListItemText, ListItemIcon,  } from '@material-ui/core'
-import {missionGuide} from '../../constants/missionInfo'
+import {
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon
+} from '@material-ui/core'
+import { missionGuide } from '../../constants/missionInfo'
 import Mission1 from '../../../../assets/images/mission1.svg'
 import Mission2 from '../../../../assets/images/mission2.svg'
 import Mission3 from '../../../../assets/images/mission3.svg'
@@ -16,13 +22,13 @@ const useStyles = makeStyles({
     top: 0,
     left: 0,
     zIndex: 1,
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center'
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   secondaryText: {
     fontSize: '0.5em',
-    color:'white'
+    color: 'white'
   }
 })
 
@@ -38,26 +44,24 @@ export default function GuidePageStep2() {
         功能說明
       </Typography>
       <List>
-      {missionGuide.map((item, index) => (
-        <ListItem
-          button
-        >
-          <ListItemIcon>
-            <img
-              src={missionImage[index]}
-              alt=''
-              style={{ height: '50px', width: '50px' }}
+        {missionGuide.map((item, index) => (
+          <ListItem>
+            <ListItemIcon>
+              <img
+                src={missionImage[index]}
+                alt=''
+                style={{ height: '50px', width: '50px' }}
+              />
+            </ListItemIcon>
+            <ListItemText
+              classes={{ secondary: classes.secondaryText }}
+              primary={item.missionName}
+              secondary={item.missionDescription}
+              style={{ color: 'white', fontSize: '2em' }}
             />
-          </ListItemIcon>
-          <ListItemText
-            classes={{ secondary: classes.secondaryText }}
-            primary={item.missionName}
-            secondary={item.missionDescription}
-            style={{color:'white', fontSize:'2em'}}
-          />
-        </ListItem>
-      ))}
-    </List>
+          </ListItem>
+        ))}
+      </List>
     </div>
   )
 }
