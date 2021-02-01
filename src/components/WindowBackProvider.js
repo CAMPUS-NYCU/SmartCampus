b84edf6e8bb9f30bc034478775ba82fb1c83ce18
KalleChen <chen1082yo@gmail.com>
2021-02-01 17:54:34 +0800

    Add mission drawer back funtion

--- src/components/WindowBackProvider.js
+++ src/components/WindowBackProvider.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { useTagValue } from '../pages/MapPage/contexts/TagContext'
+import { useMissionValue } from '../pages/MapPage/contexts/MissionContext'
 
-export default function WindowBackProvider(props) {
+export default function WindowBackProvider (props) {
   const { activeTag, resetActiveTag } = useTagValue()
+  const { isInMission, handleCloseMission } = useMissionValue()
   React.useEffect(() => {
     window.history.pushState(null, null, window.location.pathname)
-    const handleBackButton = (e) => {
+    const handleBackButton = e => {
       e.preventDefault()
       window.history.pushState(null, null, window.location.pathname)
     }
-    window.addEventListener('popstate', (e) => handleBackButton(e), false)
-    return window.removeEventListener('popstate', (e) => handleBackButton(e))
+    window.addEventListener('popstate', e => handleBackButton(e), false)
+    return window.removeEventListener('popstate', e => handleBackButton(e))
   }, [])
   React.useEffect(() => {
     window.addEventListener(
@@ -20,20 +22,28 @@ export default function WindowBackProvider(props) {
         if (activeTag) {
           resetActiveTag()
         }
+        if (isInMission) {
+          console.log('hi')
+          handleCloseMission()
+        }
       },
       false
     )
     return window.removeEventListener(
-        'popstate',
-        () => {
-          console.log('back')
-          if (activeTag) {
-            resetActiveTag()
-          }
-        },
-        false
-      )
-  },[activeTag, resetActiveTag])
+      'popstate',
+      () => {
+        console.log('back')
+        if (activeTag) {
+          resetActiveTag()
+        }
+        if (isInMission) {
+          console.log('hi')
+          handleCloseMission()
+        }
+      },
+      false
+    )
+  }, [activeTag, resetActiveTag, isInMission, handleCloseMission])
 
   return null
 }
