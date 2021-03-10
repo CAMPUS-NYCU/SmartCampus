23d0ba282cd2b0acd0dae671cdacc84b90a5ba36
KalleChen <chen1082yo@gmail.com>
2021-03-10 08:43:53 +0800

    Fix mission2 icon without exclamation mark

--- src/pages/MapPage/components/GuidePage/GuidePageStep2.js
+++ src/pages/MapPage/components/GuidePage/GuidePageStep2.js
@@ -10,7 +10,7 @@ import {
 } from '@material-ui/core'
 import { missionGuide } from '../../constants/missionInfo'
 import Mission1 from '../../../../assets/images/mission1circle.svg'
-import Mission2 from '../../../../assets/images/mission2circle.svg'
+import Mission2 from '../../../../assets/images/mission2-icon.svg'
 import Mission3 from '../../../../assets/images/mission3circle.svg'
 
 const useStyles = makeStyles({
