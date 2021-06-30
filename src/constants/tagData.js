f02fc1e6b6286374265aaefa3563af91f9034484
unknown <s100242005.me06@g2.nctu.edu.tw>
2021-06-30 22:06:30 +0800

    Add icon to ChangeStatus

--- src/constants/tagData.js
+++ src/constants/tagData.js
@@ -1,16 +1,35 @@
+import SlovedIcon from 'assets/images/statusSlovedIcon.svg'
+import UnslovedIcon from 'assets/images/statusUnslovedIcon.svg'
+import FewPeopleIcon from 'assets/images/statusFewPeopleIcon.svg'
+import LittleMorePeopleIcon from 'assets/images/statusLittleMorePeopleIcon.svg'
+import CrowdedIcon from 'assets/images/statusCrowdedIcon.svg'
+import WiFiGoodIcon from 'assets/images/statuWi-FiGoodIcon.svg'
+import WiFiNormalIcon from 'assets/images/statusWi-FiNormalIcon.svg'
+import WiFiBadIcon from 'assets/images/statusWi-FiBadIcon.svg'
+
 export default [
   [
     {
       statusName: '存在',
-      statusColor: '#dce775'
+      statusColor: '#dce775',
+      statusIcon: SlovedIcon
     },
-    { statusName: '資訊有誤', statusColor: '#FFB59F' }
+    {
+      statusName: '資訊有誤',
+      statusColor: '#FFB59F',
+      statusIcon: UnslovedIcon
+    }
   ],
   [
-    { statusName: '待處理', statusColor: '#FFB59F' },
+    {
+      statusName: '待處理',
+      statusColor: '#FFB59F',
+      statusIcon: UnslovedIcon
+    },
     {
       statusName: '已解決',
-      statusColor: '#dce775'
+      statusColor: '#dce775',
+      statusIcon: SlovedIcon
     }
   ],
   [
@@ -25,13 +44,21 @@ export default [
     { statusName: '良好', statusColor: '#dce775' }
   ],
   [
-    { statusName: '人少', statusColor: '#dce775' },
-    { statusName: '人稍多', statusColor: '#FFCC80' },
-    { statusName: '擁擠', statusColor: '#FFB59F' }
+    { statusName: '人少', statusColor: '#dce775', statusIcon: FewPeopleIcon },
+    {
+      statusName: '人稍多',
+      statusColor: '#FFCC80',
+      statusIcon: LittleMorePeopleIcon
+    },
+    { statusName: '擁擠', statusColor: '#FFB59F', statusIcon: CrowdedIcon }
   ],
   [
-    { statusName: '良好', statusColor: '#dce775' },
-    { statusName: '正常', statusColor: '#FFCC80' },
-    { statusName: '微弱', statusColor: '#FFB59F' }
+    { statusName: '良好', statusColor: '#dce775', statusIcon: WiFiGoodIcon },
+    {
+      statusName: '正常',
+      statusColor: '#FFCC80',
+      statusIcon: WiFiNormalIcon
+    },
+    { statusName: '微弱', statusColor: '#FFB59F', statusIcon: WiFiBadIcon }
   ]
 ]
