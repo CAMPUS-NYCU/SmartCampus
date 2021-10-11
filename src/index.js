1382833353a5e5b001b1009175908c36b81bb130
Kalle Chen <chen1082yo@gmail.com>
2021-10-11 22:00:40 +0800

    Add sentry setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import * as Sentry from '@sentry/react'
+import { Integrations } from '@sentry/tracing'
+
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN || '',
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0
+})
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
