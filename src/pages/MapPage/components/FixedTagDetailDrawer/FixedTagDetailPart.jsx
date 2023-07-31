4966e6c9661388f05c640e80305b8b65dddb2152
y-c-wang <ycwang.cs@gmail.com>
2023-07-31 23:16:54 +0800

    fixed tag combine floors

--- src/pages/MapPage/components/FixedTagDetailDrawer/FixedTagDetailPart.jsx
+++ src/pages/MapPage/components/FixedTagDetailDrawer/FixedTagDetailPart.jsx
@@ -1,13 +1,11 @@
-import React, { useMemo, useState } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { Box, Button } from '@mui/material'
 import Grid from '@mui/material/Grid'
 import { MAP_PATH } from '../../../../constants/pageUrls'
 import changeImage from '../../../../assets/images/fixedTagChange.svg'
-import CustomButton from '../../../../components/CustomButton'
 import { useTagValue } from '../../../../utils/contexts/TagContext'
-import { fixedTagContext } from '../../../../constants/fixedTagContext'
 
 const DetailPartItem = (props) => {
   const { tag } = props
@@ -54,7 +52,29 @@ const DetailPartItem = (props) => {
           textTransform: 'uppercase'
         }}
       >
-        {tag.locationName}
+        地點: {tag.locationName}
+      </Grid>
+      <Grid
+        item
+        xs={4}
+        style={{
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        {tag.floor && (
+          <Button
+            id='floorButton'
+            size='small'
+            style={{
+              borderRadius: '20px',
+              boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.12)'
+            }}
+            variant='contained'
+          >
+            樓層: {tag.floor}
+          </Button>
+        )}
       </Grid>
       <Grid
         item
@@ -118,78 +138,57 @@ const DetailPartItem = (props) => {
   )
 }
 
-const DetailPartContext = (props) => {
+const DetailPartPlaceholder = () => {
+  return (
+    <Box
+      display='flex'
+      flexDirection='column'
+      m={2}
+      style={{
+        overflowX: 'hidden',
+        overflowY: 'scroll',
+        height: '80%',
+        width: '90%',
+        marginTop: '10px'
+      }}
+    >
+      [TODO]: 無資料
+    </Box>
+  )
+}
+
+const DetailPartWithContext = (props) => {
   const { tags } = props
-  return tags.map((tag) => <DetailPartItem key={tag.id} tag={tag} />)
+  return (
+    <Box
+      display='flex'
+      flexDirection='column'
+      m={2}
+      style={{
+        overflowX: 'hidden',
+        overflowY: 'scroll',
+        height: '80%',
+        width: '90%',
+        marginTop: '10px'
+      }}
+    >
+      {tags.map((tag) => (
+        <DetailPartItem key={tag.id} tag={tag} />
+      ))}
+    </Box>
+  )
 }
 
 const DetailPart = (props) => {
-  const { activeFixedTag } = props
-  const [floor, setFloor] = useState(
-    activeFixedTag.locationName === '圖書館' ? '2F' : '1F'
-  )
-
-  const { fixedtagDetail } = useTagValue()
-  const { information } = useMemo(
-    () =>
-      fixedTagContext.find(
-        (fixedtagfloor) =>
-          fixedtagfloor.locationName === activeFixedTag.locationName
-      ) || {},
-    [activeFixedTag]
-  )
-  return (
-    <>
-      <Box
-        display='flex'
-        alignItems='center'
-        flexDirection='row'
-        m={2}
-        style={{
-          overflowY: 'hidden',
-          overflowX: 'scroll',
-          height: '15%',
-          width: '90%',
-          margin: '2px'
-        }}
-      >
-        {information?.map((discovery) => (
-          <CustomButton
-            key={discovery.floor}
-            buttonType={
-              discovery.floor === floor
-                ? 'fixedTagChosefloorButton'
-                : 'fixedTagfloorButton'
-            }
-            onClick={() => setFloor(discovery.floor)}
-          >
-            {discovery.floor}
-          </CustomButton>
-        ))}
-      </Box>
-      <Box
-        display='flex'
-        flexDirection='column'
-        m={2}
-        style={{
-          overflowX: 'hidden',
-          overflowY: 'scroll',
-          height: '80%',
-          width: '90%',
-          marginTop: '10px'
-        }}
-      >
-        {fixedtagDetail.tags.length > 0 ? (
-          <DetailPartContext tags={fixedtagDetail.tags} />
-        ) : (
-          '[TODO]: 無資料'
-        )}
-      </Box>
-    </>
-  )
+  const { tags } = props
+  if (tags.length === 0) {
+    return <DetailPartPlaceholder />
+  }
+  return <DetailPartWithContext tags={tags} />
 }
+
 DetailPart.propTypes = {
-  activeFixedTag: PropTypes.object.isRequired
+  tags: PropTypes.array.isRequired
 }
 
 export default DetailPart
