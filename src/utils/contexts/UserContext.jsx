575177a7d32fce8f8cf4bc4383b4d609502768d9
y-c-wang <ycwang.cs@gmail.com>
2023-08-22 23:16:42 +0800

    add SignInWithEmailAndPassword

--- src/utils/contexts/UserContext.jsx
+++ src/utils/contexts/UserContext.jsx
@@ -80,6 +80,7 @@ const reducer = (user, action) => {
 const initialUserValue = {
   ...initialUser,
   signInWithGuest: () => {},
+  signInWithEmailAndPassword: async () => {},
   signInWithGoogle: () => {},
   signOut: () => {},
   isGuest: false
@@ -89,89 +90,98 @@ const UserContext = createContext(initialUserValue)
 export const UserContextProvider = withFirebaseAuth({
   providers,
   firebaseAppAuth
-})(({ children, user, signInWithGoogle, signOut }) => {
-  const [userInfo, dispatch] = useReducer(reducer, initialUser)
-  const signInWithGuest = () => {
-    dispatch({
-      type: actionTypes.setToken,
-      payload: 'guest'
-    })
-  }
-  const [isLoadingToken, setIsLoadingToken] = useState(true)
-  const handleGetUserInfo = useCallback(async () => {
-    if (user === null) setIsLoadingToken(false)
-    if (user) {
-      dispatch({
-        type: actionTypes.setUserName,
-        payload: user.displayName
-      })
-      dispatch({
-        type: actionTypes.setUserEmail,
-        payload: user.email
-      })
-      dispatch({
-        type: actionTypes.setUserPicture,
-        payload: user.photoURL
-      })
-      const token = await user.getIdToken()
-      dispatch({
-        type: actionTypes.setToken,
-        payload: token
-      })
-      setIsLoadingToken(false)
-      dispatch({
-        type: actionTypes.setUid,
-        payload: user.uid
-      })
-    }
-  }, [user])
-  const delay = (ms) => new Promise((res) => setTimeout(res, ms))
-  const handleRefreshToken = useCallback(async () => {
-    const expireTime = getItem(TOKEN_EXPIRE_INFO)
-    if (expireTime) {
-      const timeout = REFRESH_TOKEN_TIMEOUT - (Date.now() - expireTime)
-      await delay(timeout)
-    }
-    if (user) {
-      const token = await user.getIdToken()
-      dispatch({
-        type: actionTypes.setToken,
-        payload: token
-      })
-      setItem(TOKEN_EXPIRE_INFO, Date.now())
-    }
-    handleRefreshToken()
-    // every 15 minutes, refresh token
-  }, [user])
-  const handleSignOut = () => {
-    dispatch({ type: actionTypes.cleanUser })
-    signOut()
-  }
-  useEffect(() => {
-    handleGetUserInfo()
-  }, [handleGetUserInfo])
-  useEffect(() => {
-    if (user) handleRefreshToken()
-  }, [handleRefreshToken, user])
-  useEffect(() => {
-    if (VITE_FIREBASE_LOCAL_SERVER) {
-      firebaseAppAuth.useEmulator(VITE_FIREBASE_EMULATER_URL)
-    }
-  }, [])
-  const contextValues = {
-    ...userInfo,
-    signInWithGuest,
+})(
+  ({
+    children,
+    user,
+    signInWithEmailAndPassword,
     signInWithGoogle,
-    signOut: handleSignOut,
-    isGuest: userInfo.token === 'guest',
-    isLoadingToken,
-    handleRefreshToken
+    signOut
+  }) => {
+    const [userInfo, dispatch] = useReducer(reducer, initialUser)
+    const signInWithGuest = () => {
+      dispatch({
+        type: actionTypes.setToken,
+        payload: 'guest'
+      })
+    }
+    const [isLoadingToken, setIsLoadingToken] = useState(true)
+    const handleGetUserInfo = useCallback(async () => {
+      if (user === null) setIsLoadingToken(false)
+      if (user) {
+        dispatch({
+          type: actionTypes.setUserName,
+          payload: user.displayName
+        })
+        dispatch({
+          type: actionTypes.setUserEmail,
+          payload: user.email
+        })
+        dispatch({
+          type: actionTypes.setUserPicture,
+          payload: user.photoURL
+        })
+        const token = await user.getIdToken()
+        dispatch({
+          type: actionTypes.setToken,
+          payload: token
+        })
+        setIsLoadingToken(false)
+        dispatch({
+          type: actionTypes.setUid,
+          payload: user.uid
+        })
+      }
+    }, [user])
+    const delay = (ms) => new Promise((res) => setTimeout(res, ms))
+    const handleRefreshToken = useCallback(async () => {
+      const expireTime = getItem(TOKEN_EXPIRE_INFO)
+      if (expireTime) {
+        const timeout = REFRESH_TOKEN_TIMEOUT - (Date.now() - expireTime)
+        await delay(timeout)
+      }
+      if (user) {
+        const token = await user.getIdToken()
+        dispatch({
+          type: actionTypes.setToken,
+          payload: token
+        })
+        setItem(TOKEN_EXPIRE_INFO, Date.now())
+      }
+      handleRefreshToken()
+      // every 15 minutes, refresh token
+    }, [user])
+    const handleSignOut = () => {
+      dispatch({ type: actionTypes.cleanUser })
+      signOut()
+    }
+    useEffect(() => {
+      handleGetUserInfo()
+    }, [handleGetUserInfo])
+    useEffect(() => {
+      if (user) handleRefreshToken()
+    }, [handleRefreshToken, user])
+    useEffect(() => {
+      if (VITE_FIREBASE_LOCAL_SERVER) {
+        firebaseAppAuth.useEmulator(VITE_FIREBASE_EMULATER_URL)
+      }
+    }, [])
+    const contextValues = {
+      ...userInfo,
+      signInWithGuest,
+      signInWithEmailAndPassword,
+      signInWithGoogle,
+      signOut: handleSignOut,
+      isGuest: userInfo.token === 'guest',
+      isLoadingToken,
+      handleRefreshToken
+    }
+    return (
+      <UserContext.Provider value={contextValues}>
+        {children}
+      </UserContext.Provider>
+    )
   }
-  return (
-    <UserContext.Provider value={contextValues}>
-      {children}
-    </UserContext.Provider>
-  )
-})
+)
 
 export const useUserValue = () => useContext(UserContext)
