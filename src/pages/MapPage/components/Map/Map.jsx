00a859cdb481e1a24407b109f1e945da903b2aa3
hsinlun99 <hsinlun.chiu@gmail.com>
2023-09-22 14:53:34 +0800

    yellow marker upper shift while creating done

--- src/pages/MapPage/components/Map/Map.jsx
+++ src/pages/MapPage/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react'
+import React, { useEffect, useCallback, useMemo } from 'react'
 import { GoogleMap, Marker } from '@react-google-maps/api'
 import { MarkerClusterer } from '@googlemaps/markerclusterer'
 import { useHistory } from 'react-router-dom'
@@ -17,24 +17,6 @@ import FixedTags from '../../../../assets/images/fixedtag.svg'
 import Searchposition from '../../../../assets/images/searchPositionIcon.svg'
 import ReferMarkerFab from '../ReferMarkerFab'
 import UserLocationResearch from '../ReferMarkerFab/UserLocationResearch'
-// import markerCleanIcon from '../../../../assets/images/res1-statusMarkerClean.svg'
-// import markerCrowdIcon from '../../../../assets/images/res1-statusMarkerCrowd.svg'
-// import markerFunctionIcon from '../../../../assets/images/res1-statusMarkerFunction.svg'
-// import markerMaintenanceIcon from '../../../../assets/images/res1-statusMarkerMaintenance.svg'
-// import markerNoiseIcon from '../../../../assets/images/res1-statusMarkerNoise.svg'
-// import markerOccupationIcon from '../../../../assets/images/res1-statusMarkerOccupation.svg'
-// import markerOutlookIcon from '../../../../assets/images/res1-statusMarkerOutlook.svg'
-// import markerThermalComfortIcon from '../../../../assets/images/res1-statusMarkerThermalComfort.svg'
-// import markerUsageIcon from '../../../../assets/images/res1-statusMarkerUsage.svg'
-// import markerCleanActivatedIcon from '../../../../assets/images/res1-statusMarkerCleanActivated.svg'
-// import markerCrowdActivatedIcon from '../../../../assets/images/res1-statusMarkerCrowdActivated.svg'
-// import markerFunctionActivatedIcon from '../../../../assets/images/res1-statusMarkerFunctionActivated.svg'
-// import markerMainenanceActiatedIcon from '../../../../assets/images/res1-statusMarkerMaintenanceActivated.svg'
-// import markerNoiseActivatedIcon from '../../../../assets/images/res1-statusMarkerNoiseActivated.svg'
-// import markerOccupationActivatedIcon from '../../../../assets/images/res1-statusMarkerOccupationActivated.svg'
-// import markerOutlookActivatedIcon from '../../../../assets/images/res1-statusMarkerOutlookActivated.svg'
-// import markerThermalComfortActivatedIcon from '../../../../assets/images/res1-statusMarkerThermalComfortActivated.svg'
-// import markerUsageActivatedIcon from '../../../../assets/images/res1-statusMarkerUsageActivated.svg'
 import { markerIcon } from '../../../../constants/res1MarkerIcon'
 
 function Map(props) {
@@ -69,49 +51,7 @@ function Map(props) {
     activeFixedTag
   } = useTagValue()
 
-  // const missionImage = useMemo(() => [Mission1, Mission2], [])
-  // const statusName = useMemo(
-  //   () => [
-  //     '清潔狀態',
-  //     '人潮狀態',
-  //     '功能狀態',
-  //     '保養狀態',
-  //     '噪音狀態',
-  //     '佔用狀態',
-  //     '外觀狀態',
-  //     '體感狀態',
-  //     '使用狀態'
-  //   ],
-  //   []
-  // )
-  // const displayMarkerIcon = useMemo(
-  //   () => [
-  //     markerCleanIcon,
-  //     markerCrowdIcon,
-  //     markerFunctionIcon,
-  //     markerMaintenanceIcon,
-  //     markerNoiseIcon,
-  //     markerOccupationIcon,
-  //     markerOutlookIcon,
-  //     markerThermalComfortIcon,
-  //     markerUsageIcon
-  //   ],
-  //   []
-  // )
-  // const highlightMarkerIcon = useMemo(
-  //   () => [
-  //     markerCleanActivatedIcon,
-  //     markerCrowdActivatedIcon,
-  //     markerFunctionActivatedIcon,
-  //     markerMainenanceActiatedIcon,
-  //     markerNoiseActivatedIcon,
-  //     markerOccupationActivatedIcon,
-  //     markerOutlookActivatedIcon,
-  //     markerThermalComfortActivatedIcon,
-  //     markerUsageActivatedIcon
-  //   ],
-  //   []
-  // )
+  const markerStatusIcon = useMemo(() => markerIcon.map((markerInfo) => {return markerInfo}), [])
 
   const isShown = useCallback(
     (tag) => {
@@ -141,17 +81,17 @@ function Map(props) {
     (tag) => {
       if (highlightTagId === tag.id) {
         return {
-          url: markerIcon[
-            markerIcon.findIndex(
-              (icon) => icon.status === tag.status.statusName
+          url: markerStatusIcon[
+            markerStatusIcon.findIndex(
+              (markerInfo) => markerInfo.status === tag.status.statusName
             )
           ].highlightIcon,
           scaledSize: { width: 42, height: 45 }
         }
       }
       return {
-        url: markerIcon[
-          markerIcon.findIndex((icon) => icon.status === tag.status.statusName)
+        url: markerStatusIcon[
+          markerStatusIcon.findIndex((markerInfo) => markerInfo.status === tag.status.statusName)
         ].normalIcon,
         scaledSize: { width: 28, height: 30 }
       }
--- src/utils/contexts/MissionContext.jsx
+++ src/utils/contexts/MissionContext.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useMemo, useCallback } from 'react'
+import React, { useContext, useState, useMemo, useCallback, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useSnackbar } from 'notistack'
 import { gql, useMutation } from '@apollo/client'
@@ -181,7 +181,7 @@ export const MissionContextProvider = ({ children }) => {
   const handleSetMarkerPosition = useCallback(() => {
     setMarkerPosition({
       longitude: mapInstance.getCenter().lng(),
-      latitude: mapInstance.getCenter().lat()
+      latitude: mapInstance.getBounds()?.Ua?.hi - (mapInstance?.getBounds()?.Ua?.hi - mapInstance?.getBounds()?.Ua?.lo)/4
     })
   }, [mapInstance])
 
@@ -244,7 +244,7 @@ export const MissionContextProvider = ({ children }) => {
     const center = mapInstance.getCenter()
     setMarkerPosition({
       longitude: center.lng(),
-      latitude: center.lat()
+      latitude: mapInstance.getBounds()?.Ua?.hi - (mapInstance?.getBounds()?.Ua?.hi - mapInstance?.getBounds()?.Ua?.lo)/4
     })
     setStep(MISSION_FIRST_STEP)
   }, [mapInstance, setStep])
@@ -312,7 +312,7 @@ export const MissionContextProvider = ({ children }) => {
         locationImgUrl: []
       },
       coordinates: {
-        latitude: (markerPosition.latitude+0.0001).toString(),
+        latitude: (markerPosition.latitude).toString(),
         longitude: markerPosition.longitude.toString()
       },
       imageUploadNumber: imageFiles.length,
