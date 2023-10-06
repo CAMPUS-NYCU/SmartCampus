4e9301463c699acb59d9121979c59e2e647f76d6
hsinlun99 <hsinlun.chiu@gmail.com>
2023-10-06 10:39:05 +0800

    upper shift highlighted tagData

--- src/utils/contexts/MissionContext.jsx
+++ src/utils/contexts/MissionContext.jsx
@@ -169,7 +169,10 @@ export const MissionContextProvider = ({ children }) => {
 
   const handlePanTo = useCallback(
     (latlng) => {
-      mapInstance.panTo(latlng)
+      mapInstance.panTo({
+        lat: latlng.lat - (mapInstance?.getBounds()?.rb?.hi - mapInstance?.getBounds()?.rb?.lo) / 4,
+        lng: latlng.lng
+      })
     },
     [mapInstance]
   )
